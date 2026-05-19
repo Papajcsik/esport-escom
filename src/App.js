1d264a42fdcdaf78dfac54714f86a400ad264d6e
strahinja2112 <pavicevicstrahinja99@gmail.com>
2026-05-19 10:50:06 +0200

    basic formatting done, test for commiting

--- src/App.js
+++ src/App.js
@@ -1,134 +1,293 @@
-import { colors } from './Constants';
-import './App.css';
-import { IoMenu } from 'react-icons/io5'
-import { AiOutlineHome, AiOutlineClose } from 'react-icons/ai'
-import { GoMail } from 'react-icons/go'
-import { BsQuestionCircle } from 'react-icons/bs'
-import { BiLogoFacebook, BiLogoTwitter, BiLogoInstagram, BiLogoYoutube } from 'react-icons/bi'
-import { useState } from 'react';
-import Contact from './pages/Contact';
-import WhatIsESCOM from './pages/WhatIsESCOM';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineHome } from "react-icons/ai";
+import {
+	BiLogoFacebook,
+	BiLogoInstagram,
+	BiLogoTwitter,
+	BiLogoYoutube,
+} from "react-icons/bi";
+import { BsQuestionCircle } from "react-icons/bs";
+import { GoMail } from "react-icons/go";
+import { IoMenu } from "react-icons/io5";
+import "./App.css";
+import { colors } from "./Constants";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import WhatIsESCOM from "./pages/WhatIsESCOM";
 
 function App() {
+	const [menuShown, setMenuShown] = useState(false);
 
-  const [menuShown, setMenuShown] = useState(false);
+	const IMAGES = {
+		titan1: new URL("../src/images/escom-titan-hatter.webp", import.meta.url)
+			.href,
+		logo1: new URL(
+			"../src/images/escom_logo-SEC-COMMAND2-veryion.webp",
+			import.meta.url,
+		).href,
+		qrCode: new URL("../src/images/frame.png", import.meta.url).href,
+		guruLogo: new URL(
+			"../src/images/Guru-logo-escom-menupoint.webp",
+			import.meta.url,
+		).href,
+	};
 
-const IMAGES = {
-    titan1 : new URL('../src/images/escom-titan-hatter.webp', import.meta.url).href,
-    logo1 : new URL('../src/images/escom_logo-SEC-COMMAND2-veryion.webp', import.meta.url).href,
-    qrCode: new URL('../src/images/frame.png', import.meta.url).href,
-    guruLogo: new URL('../src/images/Guru-logo-escom-menupoint.webp', import.meta.url).href,
+	let Component;
+	switch (window.location.pathname) {
+		case "/":
+			Component = Home;
+			break;
+		case "/Contact":
+			Component = Contact;
+			break;
+		case "/WhatIsESCOM":
+			Component = WhatIsESCOM;
+			break;
+		default:
+			Component = NotFound;
+			break;
+	}
 
-    
-}
+	const handleGooglePlayLink = () => {
+		const mobileCheck =
+			/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+				navigator.userAgent,
+			);
+		const playStoreUrl =
+			"https://play.google.com/store/apps/details?id=com.escom";
+		const newWindow = window.open();
 
-  let Component;
-  switch(window.location.pathname)
-  {
-    case "/":
-      Component = Home;
-      break;
-    case "/Contact":
-      Component = Contact;
-      break;
-    case "/WhatIsESCOM":
-      Component = WhatIsESCOM;
-      break;
-    default: 
-      Component = NotFound;
-    break;
-  }
+		if (mobileCheck) {
+			// Mobile - try to open Play Store app first, fallback to browser
+			newWindow.location.href = `market://details?id=com.escom`;
+			setTimeout(() => {
+				if (!newWindow.closed) {
+					newWindow.location.href = playStoreUrl;
+				}
+			}, 500);
+		} else {
+			// Desktop - open in new tab
+			newWindow.location.href = playStoreUrl;
+		}
+	};
 
-  const handleGooglePlayLink = () => {
-    const mobileCheck = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    const playStoreUrl = 'https://play.google.com/store/apps/details?id=com.escom';
-    const newWindow = window.open();
-    
-    if (mobileCheck) {
-      // Mobile - try to open Play Store app first, fallback to browser
-      newWindow.location.href = `market://details?id=com.escom`;
-      setTimeout(() => {
-        if (!newWindow.closed) {
-          newWindow.location.href = playStoreUrl;
-        }
-      }, 500);
-    } else {
-      // Desktop - open in new tab
-      newWindow.location.href = playStoreUrl;
-    }
-  };
+	return (
+		<div className="App">
+			<div className="main">
+				<div className="navbar">
+					<button
+						className="hamburgerMenu"
+						onClick={() => {
+							setMenuShown(true);
+						}}
+					>
+						<IoMenu size={"95%"} color="white" />
+					</button>
 
-  return (
-    <div className="App">
+					<a href="/" className="logoContainer">
+						<img className="logo1" src={IMAGES.logo1} alt="logo1" />
+					</a>
 
-      <div className='main' >
+					<button className="signUpButton" onClick={handleGooglePlayLink}>
+						{/* <h1 className='signUpText' >Sign Up!</h1> */}
+					</button>
+				</div>
 
-        <div className='navbar'>
-          <button className='hamburgerMenu' onClick={()=>{setMenuShown(true)}} >
-            <IoMenu  size={'95%'} color='white' />
-          </button>
-          
-          <a href='/' className='logoContainer'>
-              <img className='logo1' src={IMAGES.logo1} alt='logo1' />
-          </a>
+				<div
+					className={
+						menuShown ? "menuContent menuShown" : "menuContent menuHidden"
+					}
+				>
+					<div
+						className="menuItem"
+						onClick={() => {
+							setMenuShown(false);
+						}}
+					>
+						<div className="menuItemText">
+							<AiOutlineClose
+								style={{ paddingRight: 25, color: colors.grey }}
+							/>
+						</div>
+					</div>
+					<a
+						href="/"
+						className={
+							window.location.pathname === "/" ? "menuItem active" : "menuItem"
+						}
+					>
+						<div
+							className={
+								window.location.pathname === "/"
+									? "verticalLine activeVertical"
+									: "verticalLine"
+							}
+						/>{" "}
+						<div className="menuItemText">
+							{" "}
+							<AiOutlineHome style={{ paddingRight: 25, color: colors.grey }} />
+							Home
+						</div>
+					</a>
+					<a
+						href="/Contact"
+						className={
+							window.location.pathname === "/Contact"
+								? "menuItem active"
+								: "menuItem"
+						}
+					>
+						<div
+							className={
+								window.location.pathname === "/Contact"
+									? "verticalLine activeVertical"
+									: "verticalLine"
+							}
+						/>{" "}
+						<div className="menuItemText">
+							{" "}
+							<GoMail style={{ paddingRight: 25, color: colors.grey }} />
+							Contact
+						</div>
+					</a>
+					<a
+						href="/WhatIsESCOM"
+						className={
+							window.location.pathname === "/WhatIsESCOM"
+								? "menuItem active"
+								: "menuItem"
+						}
+					>
+						<div
+							className={
+								window.location.pathname === "/WhatIsESCOM"
+									? "verticalLine activeVertical"
+									: "verticalLine"
+							}
+						/>{" "}
+						<div className="menuItemText">
+							{" "}
+							<BsQuestionCircle
+								style={{ paddingRight: 25, color: colors.grey }}
+							/>
+							What is ESCOM?
+						</div>
+					</a>
+					<div
+						className="menuItem"
+						onClick={() => {
+							window.open("https://www.gamesguru.rs/", "_blank");
+						}}
+					>
+						<div className="verticalLine" />{" "}
+						<div className="menuItemText">
+							{" "}
+							<img
+								src={IMAGES.guruLogo}
+								className="guruLogo"
+								alt="guruLogo"
+							/>{" "}
+							Gaming Shop
+						</div>
+					</div>
+				</div>
 
-           <button className='signUpButton' onClick={handleGooglePlayLink}>
-              {/* <h1 className='signUpText' >Sign Up!</h1> */}
-          </button>
-        </div>
+				<Component />
 
-          <div className={menuShown ? 'menuContent menuShown' : 'menuContent menuHidden'}>
-            <div className='menuItem' onClick={()=>{setMenuShown(false)}}>
-             <div className='menuItemText' ><AiOutlineClose style={{paddingRight: 25, color: colors.grey}} /></div>
-            </div>
-            <a href="/" className={window.location.pathname === "/" ? 'menuItem active' : 'menuItem' }>
-             <div className={window.location.pathname === "/" ? 'verticalLine activeVertical' : 'verticalLine'}/> <div className='menuItemText'> <AiOutlineHome style={{paddingRight: 25, color: colors.grey}}/>Home</div>
-            </a>
-            <a href="/Contact" className={window.location.pathname === "/Contact" ? 'menuItem active' : 'menuItem' }>
-             <div className={window.location.pathname === "/Contact" ? 'verticalLine activeVertical' : 'verticalLine'}/> <div className='menuItemText'> <GoMail style={{paddingRight: 25, color: colors.grey}}/>Contact</div>
-            </a>
-            <a href="/WhatIsESCOM" className={window.location.pathname === "/WhatIsESCOM" ? 'menuItem active' : 'menuItem' }>
-             <div className={window.location.pathname === "/WhatIsESCOM" ? 'verticalLine activeVertical' : 'verticalLine'}/> <div className='menuItemText' > <BsQuestionCircle style={{paddingRight: 25, color: colors.grey}}/>What is ESCOM?</div>
-            </a>
-            <div className='menuItem' onClick={() => { window.open('https://www.gamesguru.rs/', '_blank'); }}>
-             <div className='verticalLine'/> <div className='menuItemText'> <img src={IMAGES.guruLogo} className='guruLogo' alt='guruLogo' /> Gaming Shop</div>
-            </div>
-          </div>
+				<div className="footer">
+					<div className="footerLogoContainer">
+						<img
+							className="logoClass"
+							alt="guru"
+							src={require("../src/images/Guru_logo.png")}
+						/>
+						<img
+							className="logoClass"
+							alt="prosperitati"
+							src={require("../src/images/prospi.png")}
+						/>
+						<img
+							className="logoClass"
+							alt="sp"
+							src={require("../src/images/cover-photo.png")}
+						/>
+					</div>
 
-        <Component />
+					<div className="footertextContainer">
+						<div className="footerText">
+							Powered by:{" "}
+							<div
+								style={{ textDecoration: "underline", paddingLeft: 10 }}
+								className="hoverPink"
+								onClick={() => {
+									window.open("https://www.gamesguru.rs/", "_blank");
+								}}
+							>
+								GamesGuru
+							</div>
+						</div>
 
-        <div className='footer'>
-        
-        <div className='footerLogoContainer'>
-             <img  className='logoClass' alt='guru' src={require('../src/images/Guru_logo.png')}/>
-             <img  className='logoClass' alt='prosperitati' src={require('../src/images/prospi.png')}/>
-             <img  className='logoClass' alt='sp' src={require('../src/images/cover-photo.png')}/>
+						<div className="socialMediaContainer">
+							<a
+								className="socialMediaItem"
+								href="https://www.facebook.com/esportEscom"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<BiLogoFacebook
+									style={{ color: colors.grey, height: "80%", width: "80%" }}
+								/>
+							</a>
+							<a
+								className="socialMediaItem"
+								href="https://twitter.com/esport_escom"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<BiLogoTwitter
+									style={{ color: colors.grey, height: "80%", width: "80%" }}
+								/>
+							</a>
+							<a
+								className="socialMediaItem"
+								href="https://www.instagram.com/Esport_Escom/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<BiLogoInstagram
+									style={{ color: colors.grey, height: "80%", width: "80%" }}
+								/>
+							</a>
+							<a
+								className="socialMediaItem"
+								href="https://www.youtube.com/channel/UC9lAnxCQy5BZ1lB-cNSlO4Q?view_as=subscriber"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<BiLogoYoutube
+									style={{ color: colors.grey, height: "80%", width: "80%" }}
+								/>
+							</a>
+						</div>
 
-        </div>
-
-        <div className='footertextContainer'>
-          <div className='footerText' >Powered by:   <div style={{textDecoration: 'underline', paddingLeft: 10 }} className='hoverPink' onClick={() => { window.open('https://www.gamesguru.rs/', '_blank'); }}>GamesGuru</div></div>
-            
-            <div className='socialMediaContainer'>
-              <a className='socialMediaItem' href='https://www.facebook.com/esportEscom' target="_blank" rel="noopener noreferrer"><BiLogoFacebook style={{color: colors.grey, height: '80%', width: '80%'}}/></a>
-              <a className='socialMediaItem' href='https://twitter.com/esport_escom' target="_blank" rel="noopener noreferrer"><BiLogoTwitter style={{color: colors.grey, height: '80%', width: '80%'}}/></a>
-              <a className='socialMediaItem' href='https://www.instagram.com/Esport_Escom/' target="_blank" rel="noopener noreferrer"><BiLogoInstagram style={{color: colors.grey, height: '80%', width: '80%'}}/></a>
-              <a className='socialMediaItem' href='https://www.youtube.com/channel/UC9lAnxCQy5BZ1lB-cNSlO4Q?view_as=subscriber' target="_blank" rel="noopener noreferrer"><BiLogoYoutube style={{color: colors.grey, height: '80%', width: '80%'}}/></a>
-
-            </div>
-
-          <div className='footerText' >Code and design by:   <div style={{textDecoration: 'underline', paddingLeft: 10 }} className='hoverPink' onClick={() => { window.open('https://www.studiopresent.rs/', '_blank'); }}>StudioPresent</div></div>
-         </div>
-        </div>
-
-
-      </div>
-  
-    </div>
-  );
+						<div className="footerText">
+							Code and design by:{" "}
+							<div
+								style={{ textDecoration: "underline", paddingLeft: 10 }}
+								className="hoverPink"
+								onClick={() => {
+									window.open("https://www.studiopresent.rs/", "_blank");
+								}}
+							>
+								StudioPresent
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,168 +1,194 @@
-import React, { useEffect } from 'react'
-import '../App.css';
 import AOS from "aos";
 import "aos/dist/aos.css";
-
+import React, { useEffect } from "react";
+import "../App.css";
 
 const Home = () => {
+	const IMAGES = {
+		titan1: new URL("../images/escombot-escomweb.webp", import.meta.url).href,
+		logo1: new URL(
+			"../images/escom_logo-SEC-COMMAND2-veryion.webp",
+			import.meta.url,
+		).href,
+		qrCode: new URL("../images/frame.png", import.meta.url).href,
+		guruLogo: new URL(
+			"../images/Guru-logo-escom-menupoint.webp",
+			import.meta.url,
+		).href,
 
+		Dome: new URL("../images/dome.webp", import.meta.url).href,
+		Play: new URL("../images/play.webp", import.meta.url).href,
+		Beta: new URL("../images/beta.webp", import.meta.url).href,
+		Cyber: new URL("../images/cybercore.webp", import.meta.url).href,
+		Tree: new URL("../images/tree.webp", import.meta.url).href,
+		Contractor: new URL("../images/contractorTitan.webp", import.meta.url).href,
+		getItOnPlay: new URL("../images/getItOnPlay.webp", import.meta.url).href,
+	};
 
-const IMAGES = {
-    titan1 : new URL('../images/escombot-escomweb.webp', import.meta.url).href,
-    logo1 : new URL('../images/escom_logo-SEC-COMMAND2-veryion.webp', import.meta.url).href,
-    qrCode: new URL('../images/frame.png', import.meta.url).href,
-    guruLogo: new URL('../images/Guru-logo-escom-menupoint.webp', import.meta.url).href,
+	useEffect(() => {
+		AOS.init();
+		AOS.refresh();
 
-    Dome: new URL('../images/dome.webp', import.meta.url).href,
-    Play: new URL('../images/play.webp', import.meta.url).href,
-    Beta: new URL('../images/beta.webp', import.meta.url).href,
-    Cyber: new URL('../images/cybercore.webp', import.meta.url).href,
-    Tree: new URL('../images/tree.webp', import.meta.url).href,
-    Contractor: new URL('../images/contractorTitan.webp', import.meta.url).href,
-    getItOnPlay: new URL('../images/getItOnPlay.webp', import.meta.url).href,
+		const handleScroll = () => {
+			const scrollTop = window.scrollY; // Get the current scroll position
+			const parallaxElement = document.querySelector(".titanImage");
+			const viewportWidth = window.innerWidth;
 
-}
+			if (parallaxElement) {
+				if (viewportWidth < 430) {
+					// Parallax effect for smaller screens
+					parallaxElement.style.backgroundPositionY = `${scrollTop * 1.1}px`;
+				} else {
+					// Default parallax effect for larger screens
+					parallaxElement.style.backgroundPositionY = `${scrollTop * 0.5}px`;
+				}
+			}
+		};
 
+		// Call handleScroll on page load to set the initial position
+		handleScroll();
 
-useEffect(() => {
-  AOS.init();
-  AOS.refresh();
+		// Add scroll event listener
+		window.addEventListener("scroll", handleScroll);
 
-  const handleScroll = () => {
-    const scrollTop = window.scrollY; // Get the current scroll position
-    const parallaxElement = document.querySelector('.titanImage');
-    const viewportWidth = window.innerWidth;
+		// Cleanup event listener on unmount
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-    if (parallaxElement) {
-      if (viewportWidth < 430) {
-        // Parallax effect for smaller screens
-        parallaxElement.style.backgroundPositionY = `${scrollTop * 1.1}px`;
-      } else {
-        // Default parallax effect for larger screens
-        parallaxElement.style.backgroundPositionY = `${scrollTop * 0.5}px`;
-      }
-    }
-  };
+	const handleGooglePlayLink = () => {
+		const mobileCheck =
+			/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+				navigator.userAgent,
+			);
+		const playStoreUrl =
+			"https://play.google.com/store/apps/details?id=com.escom";
+		const newWindow = window.open();
 
-  // Call handleScroll on page load to set the initial position
-  handleScroll();
+		if (mobileCheck) {
+			// Mobile - try to open Play Store app first, fallback to browser
+			newWindow.location.href = `market://details?id=com.escom`;
+			setTimeout(() => {
+				if (!newWindow.closed) {
+					newWindow.location.href = playStoreUrl;
+				}
+			}, 500);
+		} else {
+			// Desktop - open in new tab
+			newWindow.location.href = playStoreUrl;
+		}
+	};
 
-  // Add scroll event listener
-  window.addEventListener('scroll', handleScroll);
+	return (
+		<div className="mainContent">
+			<div data-aos="fade-up" className="titanImage">
+				<div className="section1">
+					<div className="textSection" data-aos="fade-up">
+						<text>
+							Welcome to ESCOM's Official Website
+							<br />
+							<br />
+							Available now in the <br />
+							Google Play Store
+						</text>
+					</div>
+					<img
+						className="image1"
+						src={IMAGES.Dome}
+						alt="Dome"
+						data-aos="fade-up"
+					/>
+				</div>
 
-  // Cleanup event listener on unmount
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
-}, []);
+				<div className="section2">
+					<img
+						className="image1"
+						src={IMAGES.Play}
+						alt="Play"
+						data-aos="fade-up"
+					/>
+					<div className="textSection2" data-aos="fade-up">
+						<text>
+							ESCOM
+							<br />
+							is a logic and code-breaking mobile game,
+							<br />
+							with community-based role-playing elements.
+						</text>
 
+						<text className="betaText">
+							Currently we are in the
+							<br />
+							OPEN BETA phase!
+							<img alt="beta" src={IMAGES.Beta} className="beta" />
+						</text>
+					</div>
+				</div>
 
+				<div className="section1">
+					<div className="textSection" data-aos="fade-up">
+						<text>
+							Contribute to the common goal of the game, with logic puzzles that
+							are even less than a minute long. Work your way up the draft list
+							to become one of the top Contractors.
+							<br />
+							<br />
+							All this is done with exciting mini-games, without any serious
+							time requirements and completely free of in-game advertising!
+						</text>
+					</div>
+					<img
+						className="image1"
+						src={IMAGES.Cyber}
+						alt="Cyber"
+						data-aos="fade-up"
+					/>
+				</div>
 
-  const handleGooglePlayLink = () => {
-    const mobileCheck = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    const playStoreUrl = 'https://play.google.com/store/apps/details?id=com.escom';
-    const newWindow = window.open();
-    
-    if (mobileCheck) {
-      // Mobile - try to open Play Store app first, fallback to browser
-      newWindow.location.href = `market://details?id=com.escom`;
-      setTimeout(() => {
-        if (!newWindow.closed) {
-          newWindow.location.href = playStoreUrl;
-        }
-      }, 500);
-    } else {
-      // Desktop - open in new tab
-      newWindow.location.href = playStoreUrl;
-    }
-  };
+				<div className="section2">
+					<img
+						className="image1"
+						src={IMAGES.Tree}
+						alt="Tree"
+						data-aos="fade-up"
+					/>
+					<div className="textSection2" data-aos="fade-up">
+						<text>
+							Moreover, you can also contribute to real-world values ​ ​with
+							your game, as we want to protect our Homeworld with our green
+							program plan and your help by planting trees.
+						</text>
+					</div>
+				</div>
 
-  return (
-        <div className='mainContent' >
+				<div className="section1">
+					<div className="textSection" data-aos="fade-up">
+						<text>
+							Become a Contractor!
+							<br />
+							Help the community build the Titan!
+							<br />
+							<br />
+							Contribute to the protection of our Homeworld!
+						</text>
+					</div>
+					<img
+						className="image1"
+						src={IMAGES.Contractor}
+						alt="Contractor"
+						data-aos="fade-up"
+					/>
+				</div>
 
-        <div data-aos="fade-up" className="titanImage">
-            
-            <div className='section1'>
-              <div className='textSection'  data-aos="fade-up" >
-              <text>
-                Welcome to ESCOM's Official Website
-                <br/><br/>
-                Available now in the <br/>
-                Google Play Store
-              </text>
-              </div>
-              <img className='image1' src={IMAGES.Dome} alt='Dome'  data-aos="fade-up" />
-            </div>
+				<div className="getItOnPlayContainer" data-aos="fade-up">
+					<button onClick={handleGooglePlayLink}>
+						<img alt="getItOnPlay" src={IMAGES.getItOnPlay} />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-            <div className='section2'>
-              <img className='image1' src={IMAGES.Play} alt='Play'  data-aos="fade-up" />
-              <div className='textSection2'  data-aos="fade-up" >
-              <text>
-                ESCOM<br/>
-                is a logic and code-breaking mobile game,<br/>
-                with community-based role-playing elements.
-              </text>
-
-              <text className='betaText'>
-                Currently we are in the<br/>
-                OPEN BETA phase!
-                <img alt='beta' src={IMAGES.Beta} className='beta'/>
-              </text>
-
-              </div>
-            </div>
-
-            <div className='section1'>
-              <div className='textSection'  data-aos="fade-up" >
-              <text>
-                Contribute to the common goal of the game, with logic puzzles that are even
-                  less than a minute long. Work your way up the draft list to become one of the top Contractors.
-                <br/>
-                <br/>
-                All this is done with exciting mini-games, without any serious 
-                time requirements and completely free of in-game advertising!
-              </text>
-              </div>
-              <img className='image1' src={IMAGES.Cyber} alt='Cyber'  data-aos="fade-up" />
-            </div>
-
-            <div className='section2'>
-              <img className='image1' src={IMAGES.Tree} alt='Tree'  data-aos="fade-up" />
-              <div className='textSection2'  data-aos="fade-up" >
-              <text>
-                Moreover, you can also contribute to real-world values ​
-                ​with your game, as we want to protect our Homeworld with our
-                green program plan and your help by planting trees.
-              </text>
-
-              </div>
-            </div>
-
-            <div className='section1'>
-              <div className='textSection'  data-aos="fade-up" >
-              <text>
-                Become a Contractor!<br/>
-                Help the community build 
-                the Titan!
-                    <br/><br/>
-                Contribute to the 
-                protection of our 
-                Homeworld!
-              </text>
-              </div>
-              <img className='image1' src={IMAGES.Contractor} alt='Contractor' data-aos="fade-up" />
-            </div>
-
-            <div className='getItOnPlayContainer'  data-aos="fade-up" >
-                <button  onClick={handleGooglePlayLink}>
-                  <img alt='getItOnPlay'  src={IMAGES.getItOnPlay} />
-                </button>
-            </div>
-
-
-        </div>
-              
-        </div>  )
-}
-
-export default Home
+export default Home;
