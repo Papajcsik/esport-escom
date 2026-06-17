c459932fc733f66b35d5ea8cfc4d5137dd392a84
Bajusz Krisztian <bajuszkrisztian246@gmail.com>
2026-06-17 17:26:53 +0200

    inserted the new images

--- src/lib/imageMap.ts
+++ src/lib/imageMap.ts
@@ -43,6 +43,7 @@ export const backgroundImages = {
   escomInitiative: "/images/background/escom-initiative.png",
   mobileGame: "/images/background/mobile-game.png",
   treath: "/images/background/treath.png",
+  develop: "/images/background/develop.png",
 } as const;
 
 export const ICONS = [
