305226c20324b72b78f6031bf509b9b11aae87b1
strahinja2112 <pavicevicstrahinja99@gmail.com>
2026-07-11 19:06:07 +0200

    new test for prod

--- src/components/sections/ParallaxSection.tsx
+++ src/components/sections/ParallaxSection.tsx
@@ -79,7 +79,7 @@ export function ParallaxSection() {
 						className="absolute bottom-0 left-0 h-[150vh] w-full object-cover object-bottom md:h-[clamp(17.5rem,28vw,22.5rem)]"
 					/>
 
-					<div className="absolute bottom-[22.5rem] md:bottom-[clamp(12rem,42vw,15.3125rem)] left-1/2 -translate-x-1/2">
+					<div className="absolute bottom-[22.5rem] md:bottom-[clamp(12rem,42vw,15.3125rem)] left-1/2 -translate-x-1/2 ">
 						<div className="relative h-[clamp(18rem,78vw,38rem)] w-[clamp(20rem,92vw,46rem)]">
 							<img
 								ref={robotRef}
